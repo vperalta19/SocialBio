1d57985550e4156b6b66ebf36e3d54657bc40f26
vperalta19 <56236510+vperalta19@users.noreply.github.com>
2020-08-31 08:39:40 -0300

    agregando apis

--- src/node.js
+++ src/node.js
@@ -25,10 +25,20 @@ con.connect(function(err){
     });
 });
 
+//API USUARIO
 
-app.post('/registrar', function(req,res){
+app.post('/registrarUsuario', function(req,res){
     var registrar = req.body;
-    if(registrar.nombreUsuario == ""){
+    con.query('SELECT * FROM Usuarios WHERE usuario = ?', registrar.usuario, function(err,result){
+        
+        if(err){
+            throw err;
+        }
+        if (result.usuario != null){
+            return res.send('El usuario ya existe');
+        }
+    });
+    if(registrar.usuario == ""){
         return res.send('Escriba su nombre de usuario');
     }
     else if(registrar.contraseña == ""){
@@ -39,13 +49,13 @@ app.post('/registrar', function(req,res){
     }
     else{
         var usuarioArray = [
-            registrar.nombreUsuario, 
+            registrar.usuario, 
             registrar.contraseña, 
             registrar.biografia, 
             registrar.fotoDePerfil, 
             registrar.email
         ];
-        con.query('INSERT INTO Usuarios (nombreUsuario, contraseña, biografia, fotoDePerfil, email) VALUES (?,?,?,?,?)', usuarioArray, function(err,result){
+        con.query('INSERT INTO Usuarios (usuario, contraseña, biografia, fotoDePerfil, email) VALUES (?,?,?,?,?)', usuarioArray, function(err,result){
             if(err){
                 throw err;
             }
@@ -54,35 +64,159 @@ app.post('/registrar', function(req,res){
     } 
 });
 
-app.put('/actualizar/:idUsuario', function(req,res){
+app.put('/editarUsuario/:usuario', function(req,res){
     var actualizar = req.body;
-    var idUsuario = parseInt(req.params.idUsuario);
-    if(isNaN(idUsuario)){
-        return res.send('Escriba bien');
+    var usuario = req.params.usuario;
+    var usuarioArray = [
+        actualizar.usuario, 
+        actualizar.contraseña, 
+        actualizar.biografia, 
+        actualizar.fotoDePerfil, 
+        actualizar.email,
+        usuario
+    ];
+    con.query('UPDATE Usuarios SET usuario = ?, contraseña = ?, biografia = ?, fotoDePerfil = ?, email = ? WHERE usuario = ?', usuarioArray, function(err,result){
+        if(err){
+            throw err;
+        }
+        res.send('se actualizo correctamente');
+    });
+});
+
+app.get('/eliminarUsuario/:usuario', function(req,res){
+    var usuario = req.params.usuario;
+    con.query('DELETE FROM Usuarios WHERE usuario = ?', usuario, function(err,result){
+        if(err){
+            throw err;
+        }
+        res.send('se elimino correctamente');
+    });
+});
+
+//API PUBLICACIONES
+
+app.post('/publicar', function(req,res){
+    var publicar = req.body;
+    if(publicar.descripcion == "" && publicar.multimedia == ""){
+        return res.send('Debe agregar contenido a su publicacion');
     }
     else{
-        var usuarioArray = [
-            actualizar.nombreUsuario, 
-            actualizar.contraseña, 
-            actualizar.biografia, 
-            actualizar.fotoDePerfil, 
-            actualizar.email
+        var publicacionArray = [
+            publicar.descripcion,
+            publicar.multimedia,
+            publicar.usuario
         ];
-        con.query('UPDATE Usuarios SET nombreUsuario = ?, contraseña = ?, biografia = ?, fotoDePerfil = ?, email = ?', usuarioArray, function(err,result){
+        con.query('INSERT INTO Publicaciones (descripcion,multimedia,usuario) VALUES (?,?,?)', publicacionArray, function(err,result){
             if(err){
                 throw err;
             }
-            res.send('actualizar el usuario: ' + result.insertId);
+            res.send('publicacion insertada correctamente');
         });
     } 
 });
 
-app.get('/eliminar/:idUsuario', function(req,res){
-    var idUsuario = parseInt(req.params.idUsuario);
-    con.query('DELETE FROM Usuarios WHERE idUsuario == ?', idUsuario, function(err,result){
+app.put('/editarPublicacion/:idPublicacion', function(req,res){
+    var actualizar = [req.body.descripcion, req.params.idPublicacion];
+    con.query('UPDATE Publicaciones SET descripcion = ? WHERE idPublicacion = ?', actualizar, function(err,result){
         if(err){
             throw err;
         }
-        res.send('actualizar el usuario: ' + result.insertId);
+        res.send('se actualizo correctamente');
     });
 });
+
+app.get('/eliminarPublicacion/:idPublicacion', function(req,res){
+    var publicacion = req.params.idPublicacion;
+    con.query('DELETE FROM Publicaciones WHERE idPublicacion = ?', publicacion, function(err,result){
+        if(err){
+            throw err;
+        }
+        res.send('se elimino correctamente');
+    });
+});
+
+//API COMENTARIOS
+app.post('/comentar', function(req,res){
+    var comentar = req.body;
+    if(comentar.comentario == ""){
+        return res.send('Su comentario no puede estar vacio');
+    }
+    else{
+        var comentarioArray = [
+            comentar.comentario,
+            comentar.idPublicacion,
+            comentar.usuario
+        ];
+        con.query('INSERT INTO Comentarios (comentario,idPublicacion,usuario) VALUES (?,?,?)', comentarioArray, function(err,result){
+            if(err){
+                throw err;
+            }
+            res.send('comentario insertado correctamente');
+        });
+    } 
+});
+
+app.put('/editarComentario/:idComentario', function(req,res){
+    var actualizar = [req.body.comentario, req.params.idComentario];
+    con.query('UPDATE Comentarios SET comentario = ? WHERE idComentario = ?', actualizar, function(err,result){
+        if(err){
+            throw err;
+        }
+        res.send('se actualizo correctamente');
+    });
+});
+
+app.get('/eliminarComentario/:idComentario', function(req,res){
+    var comentario = req.params.idComentario;
+    con.query('DELETE FROM Comentarios WHERE idComentario = ?', comentario, function(err,result){
+        if(err){
+            throw err;
+        }
+        res.send('se elimino correctamente');
+    });
+});
+
+
+//API LIKE
+app.post('/like', function(req,res){
+    var like = req.body;
+    var likeArray = [
+        like.idPublicacion,
+        like.usuario
+    ];
+    con.query('INSERT INTO Likes (idPublicacion,usuario) VALUES (?,?)', likeArray, function(err,result){
+        if(err){
+            throw err;
+        }
+        res.send('like insertado correctamente');
+    });
+});
+
+app.get('/dislike/:idLike', function(req,res){
+    con.query('DELETE FROM Likes WHERE idLike = ?', req.params.idLike, function(err,result){
+        if(err){
+            throw err;
+        }
+        res.send('se elimino correctamente');
+    });
+});
+
+//API FEED
+app.get('/feed/:usuario', function(req,res){
+    con.query('SELECT * FROM Publicaciones WHERE usuario = ?',req.params.usuario , function(err,result){
+        if(err){
+            throw err;
+        }
+        res.send(result);
+    });
+})
+
+//API INICIO
+app.get('/inicio', function(req,res){
+    con.query('SELECT * FROM Publicaciones', function(err,result){
+        if(err){
+            throw err;
+        }
+        res.send(result);
+    });
+})
